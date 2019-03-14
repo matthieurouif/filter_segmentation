1cf58cf363f9f7b07d74c3a3c8bfff86cfb777fd
Vincent Garcia <vincentfpgarcia@gmail.com>
2019-03-14 20:00:57 +0100

    add guided filter (naive and possibly wrong implementation)

---
+++ guided_filter/trash.py
@@ -0,0 +1,14 @@
+from scipy import ndimage
+from scipy.misc import imread
+
+# # Load the image and the mask
+# img = ndimage.imread('source.tiff')
+# mask = ndimage.imread('mask.tiff')
+# # print img.shape
+# # print img.shape
+
+# # Load the image and the mask
+# img = imread('source.tiff')
+# mask = imread('mask.tiff')
+# print img.shape
+# print img.shape
